d59ec740069b6ac6c173f0184c069481f2961c6d
neovincent77 <neovincent77@gmail.com>
2026-09-15 13:42:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -180,6 +180,7 @@ mention_reply_author = config.get("mention_reply_author", default_config["mentio
 require_links = config.get("require_links", default_config["require_links"])
 
 PARAM_INDEX = {param.lower(): company for company, params in trackers.items() for param in params}
+print("STKN TEST:", PARAM_INDEX.get("stkn"))
 
 try:
     REGEX = re.compile(
